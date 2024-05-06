edb4f962bd4bd8c37576f7151e56c3cffd4d29fa
Kholoud <Kholoud@DESKTOP-O32905N>
2024-05-06 22:23:17 +0300

    Update RSA function

--- securitylibrary/RSA/RSA.cs
+++ securitylibrary/RSA/RSA.cs
@@ -10,12 +10,61 @@ namespace SecurityLibrary.RSA
     {
         public int Encrypt(int p, int q, int M, int e)
         {
-            throw new NotImplementedException();
+            long n = p * q;
+            long qn = (p - 1) * (q - 1);
+            long d = GetMultiplicativeInverse(e, qn);
+            long C = (power(M, e, n));
+            return (int)C;
         }
 
+        long power(long b, long n, long m)
+        {
+            long result = 1;
+            for (int i = 0; i < n; i++)
+            {
+                result *= b;
+                result %= m;
+            }
+            return result;
+        }
+
+        long modolu(long number, long baseN)
+        {
+            // To handle negative remainders
+            long r = number % baseN;
+            if (r < 0)
+            {
+                r += baseN;
+            }
+            return r;
+        }
+        int GetMultiplicativeInverse(long number, long baseN)
+        {
+            number = modolu(number, baseN);
+            long inverse = 0;
+            for (long i = 0; i < (long)baseN; i++)
+            {
+                long GCD = (((long)number * i) % (long)baseN);
+                if (GCD == 1)
+                {
+                    inverse = i;
+                    break;
+                }
+                else
+                {
+                    inverse = -1;
+                }
+            }
+            return (int)inverse;
+        }
         public int Decrypt(int p, int q, int C, int e)
         {
-            throw new NotImplementedException();
+            long n = p * q;
+            long qn = (p - 1) * (q - 1);
+            long d = GetMultiplicativeInverse(e, qn);
+            long M = (power(C, d, n));
+            return (int)M;
+            //throw new NotImplementedException();
         }
     }
 }
