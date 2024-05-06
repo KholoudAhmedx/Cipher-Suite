ac1abfdedebf445ef46696a7d5c6f407e84602ec
Kholoud <Kholoud@DESKTOP-O32905N>
2024-05-06 22:22:51 +0300

    Update DiffieHellman function

--- securitylibrary/DiffieHellman/DiffieHellman.cs
+++ securitylibrary/DiffieHellman/DiffieHellman.cs
@@ -12,15 +12,44 @@ namespace SecurityLibrary.DiffieHellman
     public class DiffieHellman
     {
 
-      public  int power(int f, int s, int sf)
+        public int power(int f, int s, int sf)
         {
+
             throw new NotImplementedException();
         }
 
-       public List<int> GetKeys(int q, int alpha, int xa, int xb)
+        public List<int> GetKeys(int q, int alpha, int xa, int xb)
         {
+            List<int> keys = new List<int>();
+            long ya = 1;
+            long yb = 1;
+            long ka = 1;
+            long kb = 1;
+            for (int i = 0; i < xa; i++) {
+                ya *= alpha;
+                ya = ya % q;
+            }
+           
+            for (int i = 0; i < xb; i++) { 
+                yb *= alpha;
+                yb = yb % q;
+            }
+            
+            for (int i = 0; i < xa; i++) {
+                ka *= yb;
+                ka = ka % q;
+            }
+            
+            for (int i = 0; i < xb; i++) {
+                kb *= ya;
+                kb = kb % q;
+            } 
 
-            throw new NotImplementedException();
+            keys.Add((int)ka);
+            keys.Add((int)kb);
+
+            return keys;
+            //#throw new NotImplementedException();
         }
     }
 }
