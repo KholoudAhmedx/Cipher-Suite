e6da7efdedbf52d5df76c190b5640947f819969e
Kholoud <Kholoud@DESKTOP-O32905N>
2024-05-06 22:23:05 +0300

    Update El gamal function

--- securitylibrary/ElGamal/ELGAMAL.cs
+++ securitylibrary/ElGamal/ELGAMAL.cs
@@ -20,16 +20,62 @@ namespace SecurityLibrary.ElGamal
 
         public List<long> Encrypt(int q, int alpha, int y, int k, int m)
         {
-
-            throw new NotImplementedException();
-
+            List<long> result = new List<long>();
+            long C1 = power(alpha, k, q);
+            result.Add(C1);
+            long K = power(y, k, q);
+            long C2 = (K * m) % q;
+            result.Add(C2);
+            return result;
         }
-     
+        long power(long b, long n, long m)
+        {
+            long result = 1;
+            for (int i = 0; i < n; i++)
+            {
+                result *= b;
+                result %= m;
+            }
+            return result;
+        }
+
+
         public int Decrypt(int c1, int c2, int x, int q)
         {
+            long K = power(c1, x, q);
+            int inK = GetMultiplicativeInverse(K, q);
+            int p = (c2 * inK) % q;
+            return p;
 
-            throw new NotImplementedException();
-
+        }
+        long modolu(long number, long baseN)
+        {
+            // To handle negative remainders
+            long r = number % baseN;
+            if (r < 0)
+            {
+                r += baseN;
+            }
+            return r;
+        }
+        int GetMultiplicativeInverse(long number, long baseN)
+        {
+            number = modolu(number, baseN);
+            long inverse = 0;
+            for (long i = 0; i < (long)baseN; i++)
+            {
+                long GCD = (((long)number * i) % (long)baseN);
+                if (GCD == 1)
+                {
+                    inverse = i;
+                    break;
+                }
+                else
+                {
+                    inverse = -1;
+                }
+            }
+            return (int)inverse;
         }
     }
 }
